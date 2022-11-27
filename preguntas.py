045191421b5bab0c762cec0cd2c5424ce5222ece
Juanes8a <80339256+Juanes8a@users.noreply.github.com>
2022-11-27 17:55:06 -0500

    Intento 4

--- preguntas.py
+++ preguntas.py
@@ -90,7 +90,7 @@ def pregunta_03():
                 make_column_transformer(
                     (
                         OneHotEncoder(),
-                        make_column_selector(dtype=object),
+                        make_column_selector(dtype_include=object),
                     ),
                     remainder='passthrough',
                 ),
