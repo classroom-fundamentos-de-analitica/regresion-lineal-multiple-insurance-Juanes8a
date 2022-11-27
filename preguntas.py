c5197027fc6d0e128deb57d114b7c38ad4809638
Juanes8a <80339256+Juanes8a@users.noreply.github.com>
2022-11-27 17:51:54 -0500

    Intentos

--- preguntas.py
+++ preguntas.py
@@ -27,7 +27,7 @@ def pregunta_01():
     X = df.copy()
 
     # Remueva la columna `charges` del DataFrame `X`.
-    X.drop(["charges"],axis=1)
+    X = X.drop(["charges"],axis=1)
 
     # Retorne `X` y `y`
     return X, y
@@ -72,33 +72,39 @@ def pregunta_03():
     # Importe GridSearchCV
     # Importe Pipeline
     # Importe OneHotEncoder
-    from ____ import ____
-
-    pipeline = ____(
+    from sklearn.compose import make_column_transformer
+    from sklearn.compose import make_column_selector
+    from sklearn.feature_selection import SelectKBest
+    from sklearn.feature_selection import f_regression
+    from sklearn.linear_model import LinearRegression
+    from sklearn.pipeline import Pipeline
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.preprocessing import  OneHotEncoder
+    pipeline = Pipeline(
         steps=[
             # Paso 1: Construya un column_transformer que aplica OneHotEncoder a las
             # variables categóricas, y no aplica ninguna transformación al resto de
             # las variables.
             (
                 "column_transfomer",
-                ____(
+                make_column_transformer(
                     (
-                        ____(),
-                        ____(____=____),
+                        OneHotEncoder(),
+                        make_column_selector(dtype=object),
                     ),
-                    remainder=____,
+                    remainder='passthrough',
                 ),
             ),
             # Paso 2: Construya un selector de características que seleccione las K
             # características más importantes. Utilice la función f_regression.
             (
                 "selectKBest",
-                ____(____=____),
+                SelectKBest(f_regression),
             ),
             # Paso 3: Construya un modelo de regresión lineal.
             (
-                "____",
-                ____(____),
+                "linearRegression",
+                LinearRegression(),
             ),
         ],
     )
@@ -109,19 +115,19 @@ def pregunta_03():
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
     param_grid = {
-        ____: ____(____, ____),
+        "param_grid": range(1, 11),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
     # parámetros. Use cv = 5, y como métrica de evaluación el valor negativo del
     # error cuadrático medio.
-    gridSearchCV = ____(
-        estimator=____,
-        param_grid=____,
-        cv=____,
-        scoring=____,
-        refit=____,
-        return_train_score=____,
+    gridSearchCV = GridSearchCV(
+        estimator=pipeline,
+        param_grid=param_grid,
+        cv=5,
+        scoring='neg_mean_squared_error',
+        refit=True,
+        return_train_score=False,
     )
 
     # Búsque la mejor combinación de regresores
@@ -138,7 +144,7 @@ def pregunta_04():
     """
 
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.metrics import mean_squared_error
 
     # Obtenga el pipeline optimo de la pregunta 3.
     gridSearchCV = pregunta_03()
@@ -147,20 +153,20 @@ def pregunta_04():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el modelo con los conjuntos de entrenamiento y prueba.
-    y_train_pred = ____.____(____)
-    y_test_pred = ____.____(____)
+    y_train_pred = gridSearchCV.predict(X_train)
+    y_test_pred = gridSearchCV.predict(X_test)
 
     # Compute el error cuadratico medio de entrenamiento y prueba. Redondee los
     # valores a dos decimales.
 
-    mse_train = ____(
-        _____,
-        _____,
+    mse_train = mean_squared_error(
+        y_train,
+        y_train_pred,
     ).round(2)
 
-    mse_test = ____(
-        _____,
-        _____,
+    mse_test = mean_squared_error(
+        y_test,
+        y_test_pred,
     ).round(2)
 
     # Retorne el error cuadrático medio para entrenamiento y prueba
